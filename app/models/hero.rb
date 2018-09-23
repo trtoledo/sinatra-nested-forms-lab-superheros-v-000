546dda150792fa976a0837a34c74b3cd5887f4e3
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-23 07:24:38 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -4,10 +4,14 @@ class Hero
   HEROES = []
 
   def initialize(args)
-       @name = args[:name]
-       @power = args[:power]
-       @bio = args[:bio]
-       HEROES << self
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
+    HEROES << self
+  end
+
+  def self.all
+    HEROES
   end
 
 end
