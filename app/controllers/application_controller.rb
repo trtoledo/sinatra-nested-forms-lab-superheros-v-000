9995b25cf00bf15a5c87f6b47dcdc248242330c9
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-23 06:41:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
       get '/' do
-        erb :index
+        erb :'/index'
       end
 
       post '/teams' do
