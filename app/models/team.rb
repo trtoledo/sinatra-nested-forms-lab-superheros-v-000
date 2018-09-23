3869eafe5a2480ab14477d30c320f874fb4884bf
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-23 08:05:22 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,3 @@
 class Team
-  #attr_accessor :name, :motto
-
-  # def initialize(params)
-  #   @name = params[:name]
-  #   @motto = params[:motto]
-  # end
-
-
+  attr_accessor :name, :motto
 end
